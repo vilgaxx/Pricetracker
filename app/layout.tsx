435f80d1f85a5f5caba1b83df255c4ccd39db267
Mohd Yusuf Parvez <codervilgax@gmail.com>
2023-11-13 20:04:32 +0530

    Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const spaceGrotesk = Space_Grotesk({
  })
 
 export const metadata: Metadata = {
-  title: 'Pricewise',
+  title: 'Pricetracker',
   description: 'Track product prices effortlessly and save money on your online shopping.',
 }
 
